61dcb1578363f9dda03c4da64c0b55cf3f6df24e
Aishwarya Patange <aap2239@columbia.edu>
2023-01-18 15:54:30 -0500

    episode 4

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -16,5 +16,8 @@ public abstract class Piece {
         this.pieceAlliance = pieceAlliance;
     }
 
+    public Alliance getPieceAlliance() {
+        return this.pieceAlliance;
+    }
     public abstract List<Move> calculateLegalMoves(final Board board);
 }
